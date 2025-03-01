2cd30968111a3c7e1874a21d1ddb452effe1e7ed
Manoj Kumar <gmkmanoj17@gmail.com>
2025-03-01 12:35:52 +0530

    working

--- Backend/index.js
+++ Backend/index.js
@@ -6,11 +6,7 @@ const cors = require('cors')
 const app = express()
 const port = 3100
 
-app.use(cors({
-    origin: 'https://web-productcatgory-f.onrender.com',  // Allow only your frontend domain
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-}))
+app.use(cors())
 
 
 // Serve Angular static files
